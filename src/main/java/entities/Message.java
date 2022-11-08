a315bb08ce1b33bec248adb88681cbc0474c1b25
dannylu03 <danny.lu075@gmail.com>
2022-11-07 22:02:03 -0500

    Added the base entity class for Chat and Message

---
+++ src/main/java/entities/Message.java
@@ -0,0 +1,46 @@
+package entities;
+
+import java.util.Date;
+
+public class Message {
+    private String message;
+
+    private User receiver;
+
+    private User recipient;
+
+    private Date timestamp;
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+}
