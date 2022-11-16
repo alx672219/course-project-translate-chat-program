9cf3df15ab5961bf17bb83fd90d6520ac5af5aeb
Alfred Mikhael <alfred.mikhael@mail.utoronto.ca>
2022-11-15 19:45:59 -0500

    Extract JFrame initalization as a method.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import gateways.UserRegistrationFirebaseSystem;
+import org.jetbrains.annotations.NotNull;
 import services.DBInitializer;
 import user_register_use_case.UserRegistrationGateway;
 import user_register_use_case.UserRegistrationInteractor;
@@ -10,41 +11,36 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        // Build the main program window
-        JFrame application = new JFrame("Login Example");
-        CardLayout cardLayout = new CardLayout();
-        JPanel screens = new JPanel(cardLayout);
-        application.add(screens);
-
-        // Create the parts to plug into the Use Case+Entities engine
+        // Set up Database
         try {
             new DBInitializer().init();
         } catch (Exception ignored) {
-            System.out.println("Database not initlaized");
         }
+
+        // Build the main program window
+        JFrame registerScreen = initRegisterScreen();
+        registerScreen.setVisible(true);
+
+    }
+
+    @NotNull
+    private static JFrame initRegisterScreen() {
+        JFrame application = new JFrame("Register Screen");
+        CardLayout cardLayout = new CardLayout();
+        JPanel screens = new JPanel(cardLayout);
+        application.add(screens);
 
         UserRegistrationGateway userFactory = new UserRegistrationFirebaseSystem();
         UserRegisterPresenter presenter = new UserRegisterPresenter();
         UserRegisterInputBoundary interactor = new UserRegistrationInteractor(userFactory, presenter);
         UserRegisterController userRegisterController = new UserRegisterController(interactor);
 
-        // Build the GUI, plugging in the parts
         String[] langs = {"english", "arabic", "chinese", "korean", "polish"};
         RegisterScreen registerScreen = new RegisterScreen(langs, userRegisterController);
         screens.add(registerScreen, "welcome");
         cardLayout.show(screens, "register");
         application.setSize(640, 640);
-        application.setVisible(true);
-
-        // Unused screens; we'll uncomment this later
-//        WelcomeScreen welcomeScreen = new WelcomeScreen();
-//        LoginScreen loginScreen = new LoginScreen();
-//        LoggedInScreen loggedInScreen = new LoggedInScreen();
-//        screens.add(welcomeScreen, "register");
-//        screens.add(loginScreen, "login");
-//        screens.add(loggedInScreen, "loggedIn");
-
+        return application;
     }
 
 }
