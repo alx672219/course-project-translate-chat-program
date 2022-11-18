efd3bef4e01735549af1fbd6b8524f3490db6e8d
Danny Lu <danny.lu075@gmail.com>
2022-11-18 00:25:36 -0500

    Implemented the UI the Chat Screen

--- src/main/java/views/ChatScreen.java
+++ src/main/java/views/ChatScreen.java
@@ -1,90 +1,152 @@
 package views;
 
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.DataOutputStream;
 
-public class ChatScreen extends JFrame implements ActionListener {
-    JTextArea txtA = new JTextArea();
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
-    JTextField txtF = new JTextField(15);
+public class ChatScreen {
 
-    JButton btnTransfer = new JButton("Send");
+    String      appName     = "Translation Chat App";
+    ChatScreen     mainGUI;
+    JFrame      newFrame    = new JFrame(appName);
+    JButton     sendMessage;
+    JTextField  messageBox;
+    JTextArea   chatBox;
+    JTextField  usernameChooser;
+    JFrame      preFrame;
 
-    JButton btnExit = new JButton("Close");
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    UIManager.setLookAndFeel(UIManager
+                            .getSystemLookAndFeelClassName());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                ChatScreen mainGUI = new ChatScreen();
+                mainGUI.preDisplay();
+            }
+        });
+    }
 
-    boolean isFirst=true;
+    public void preDisplay() {
+        newFrame.setVisible(false);
+        preFrame = new JFrame(appName);
+        usernameChooser = new JTextField(15);
+        JLabel chooseUsernameLabel = new JLabel("Pick a username:");
+        JButton enterServer = new JButton("Enter Chat Server");
+        enterServer.addActionListener(new enterServerButtonListener());
+        JPanel prePanel = new JPanel(new GridBagLayout());
 
-    JPanel p1 = new JPanel();
+        GridBagConstraints preRight = new GridBagConstraints();
+        preRight.insets = new Insets(0, 0, 0, 10);
+        preRight.anchor = GridBagConstraints.EAST;
+        GridBagConstraints preLeft = new GridBagConstraints();
+        preLeft.anchor = GridBagConstraints.WEST;
+        preLeft.insets = new Insets(0, 10, 0, 10);
+        // preRight.weightx = 2.0;
+        preRight.fill = GridBagConstraints.HORIZONTAL;
+        preRight.gridwidth = GridBagConstraints.REMAINDER;
 
-    public ChatScreen() {
-
-        super("Chat");
-
-
-        new Id(this);
-
-
-
-        add("Center", txtA);
-
-
-
-        p1.add(txtF);
-
-        p1.add(btnTransfer);
-
-        p1.add(btnExit);
-
-        add("South", p1);
-
-
-
-        // Creating class for messages
-
-
-
-        btnTransfer.addActionListener(this);
-
-        btnExit.addActionListener(this);
-
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        setBounds(300, 300, 350, 300);
-
-        setVisible(false);
+        prePanel.add(chooseUsernameLabel, preLeft);
+        prePanel.add(usernameChooser, preRight);
+        preFrame.add(prePanel, BorderLayout.CENTER);
+        preFrame.add(enterServer, BorderLayout.SOUTH);
+        preFrame.setSize(300, 300);
+        preFrame.setVisible(true);
 
     }
 
+    public void display() {
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
 
+        JPanel southPanel = new JPanel();
+        southPanel.setBackground(Color.BLUE);
+        southPanel.setLayout(new GridBagLayout());
 
-    public void actionPerformed(ActionEvent e){
+        messageBox = new JTextField(30);
+        messageBox.requestFocusInWindow();
 
-        String id = Id.getId();
+        sendMessage = new JButton("Send Message");
+        sendMessage.addActionListener(new sendMessageButtonListener());
 
-        if(e.getSource()==btnTransfer){// WHen pressing send button
+        chatBox = new JTextArea();
+        chatBox.setEditable(false);
+        chatBox.setFont(new Font("Serif", Font.PLAIN, 15));
+        chatBox.setLineWrap(true);
 
-            // When you press send button without typing messages
+        mainPanel.add(new JScrollPane(chatBox), BorderLayout.CENTER);
 
-            if(txtF.getText().equals("")){
+        GridBagConstraints left = new GridBagConstraints();
+        left.anchor = GridBagConstraints.LINE_START;
+        left.fill = GridBagConstraints.HORIZONTAL;
+        left.weightx = 512.0D;
+        left.weighty = 1.0D;
 
-                return;
+        GridBagConstraints right = new GridBagConstraints();
+        right.insets = new Insets(0, 10, 0, 0);
+        right.anchor = GridBagConstraints.LINE_END;
+        right.fill = GridBagConstraints.NONE;
+        right.weightx = 1.0D;
+        right.weighty = 1.0D;
 
+        southPanel.add(messageBox, left);
+        southPanel.add(sendMessage, right);
+
+        mainPanel.add(BorderLayout.SOUTH, southPanel);
+
+        newFrame.add(mainPanel);
+        newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        newFrame.setSize(470, 300);
+        newFrame.setVisible(true);
+    }
+
+    class sendMessageButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            if (messageBox.getText().length() < 1) {
+                // do nothing
+            } else if (messageBox.getText().equals(".clear")) {
+                chatBox.setText("Cleared all messages\n");
+                messageBox.setText("");
+            } else {
+                chatBox.append("<" + username + ">:  " + messageBox.getText()
+                        + "\n");
+                messageBox.setText("");
             }
+            messageBox.requestFocusInWindow();
+        }
+    }
 
-            txtA.append("["+id+"] "+ txtF.getText()+"\n");
+    String  username;
 
-            txtF.setText("");
-
-        }else{
-
-            this.dispose();
-
+    class enterServerButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            username = usernameChooser.getText();
+            if (username.length() < 1) {
+                System.out.println("No!");
+            } else {
+                preFrame.setVisible(false);
+                display();
+            }
         }
 
     }
-
 }
-
-
